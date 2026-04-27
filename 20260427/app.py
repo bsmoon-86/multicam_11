4cf77380a6881809b91f831a9b4d4ab2872f55b9
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2026-04-27 16:10:57 +0900

    update

--- 20260427/app.py
+++ 20260427/app.py
@@ -9,6 +9,9 @@ from db import MyDB
 # querys 안에 user 모듈을 로드 
 from querys import user
 
+# 라이브러리 로드 
+import pandas as pd
+
 # Flask class 생성 -> 웹 서버를 구축 기능 
 # class 생성시 생성자 함수 호출 
     # 필수 인자 1개 -> 현재 실행이 되는 파일의 이름(app.py)
@@ -36,8 +39,8 @@ def index():
     # return "<a href='https://www.google.com'>Google</a>"
     # templates 폴더 안에 html 문서를 불러와서 되돌려준다. (render_template())
     return render_template("index.html")
-    # render_template에서는 {%=변수명%}는 python 에서의 변수를 담겠다. 
-    # {{python code}} code를 인식하여 조건에 맞는 경우에만 html를 추가 
+    # render_template에서는 {{변수명}}는 python 에서의 변수를 담겠다. 
+    # {%python code%} code를 인식하여 조건에 맞는 경우에만 html를 추가 
     #                  반복문을 이용해서 html를 반복적으로 추가 
 
 
@@ -92,7 +95,50 @@ def login2():
     # 데이터가 존재하지 않은 경우( 로그인 실패 ) -> () --> bool 형태로 변환 -> False
     print(sql_result)
     if sql_result:
-        return render_template('main.html')
+        # 로그인이 성공했을때는 특정 데이터를 html과 같이 유저에게 보낸다. 
+        # 로그인을 한 계정의 이름을 화면에 표시 
+        # 로그인을 한 계정의 이름은 -> 
+        # sql_result : [{'id': 'admin', 'password': '1234', 'name': 'kim'}]
+        # 리스트에서 특정 위치를 선택  -> sql_result[0] -> {'id': 'admin', 'password': '1234', 'name': 'kim'}
+        # 딕셔너리에서 특정 키를 선택 -> sql_reulst[0]['name'] -> 'kim'
+        sql_name = sql_result[0]['name']
+        # render_template( 첫번째 인자는 html문서, html문서에서 사용할 변수명 = 데이터, ... ) -> html 문서를 문자형으로 변환
+        # html 문서를 불려오면서 name이라는 변수를 사용해서 html문서 안에 대입 
+        # x축의 데이터와, y축의 데이터를 python에서 생성하고 -> html 문서에 대입 -> 그래프의 모양도 변할수 있다. 
+        # 주의 사항 -> x축의 길이와 y축의 길이가 같아야한다. -> 데이터의 형태는 list로 사용
+        # x_data = ['국어', '영어', '수학', '사회', '과학']
+        # y_data = [80, 75, 90, 85, 100]
+        # csv파일 로드 -> 데이터프레임으로 로드 
+        df = pd.read_csv("../csv/aapl.csv")
+        # 하위의 20개의 데이터를 추출
+        df2 = df.tail(10)
+        # df2.drop('Close', axis=1, inplace=True)
+        # x축의 데이터 -> Date 컬럼
+        x_data = df2['Date'].tolist()    # 데이터프레임에서 특정 컬럼을 하나 선택 -> Series(html 사용이 불가능) -> list형으로 변환
+        # y축의 데이터 -> Adj Close 컬럼    
+        y_data = df2['Adj Close'].tolist()
+        # table 태그에서 들어갈 데이터(2번째 행부터의 데이터)들을 미리 생성 
+        # 데이터프레임을 딕셔너리 형태로 변환 
+        # [ 
+        #   {'Date' : xxxxxx, 
+        #   'Open' : xxx, 
+        #   'High' : xxxx, 
+        #   'Low' : xxxx, 
+        #   'Close' : xxx, 
+        #   'Adj Close' : xxxxx, 
+        #   'Volumn' : xxxxx}, 
+        # {} ]
+        # 테이블의 컬럼의 이름들을 따로 추출해서 list 형태로 변환 
+        th_data = df2.columns.tolist()
+        # th_data -> ['Date', 'Open', ....]
+        td_data = df2.to_dict('records')
+        return render_template('main.html', 
+                               name = sql_name, 
+                               x = x_data, 
+                               y = y_data, 
+                               th_data = th_data, 
+                               td_data = td_data
+                               )
     else:
         return redirect('/')
     
